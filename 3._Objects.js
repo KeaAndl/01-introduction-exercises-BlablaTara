23ca67d6de7ffe4146eead537de254735cf75124
BlablaTara <tara0561@stud.kea.dk>
2025-02-05 23:35:52 +0100

    Objects Opgave færdig

--- 3._Objects.js
+++ 3._Objects.js
@@ -7,10 +7,26 @@ const myObject = { message: "Hello, earthling! I bring peace." };
 
 // Log the message 
 
+console.log(myObject.message);
+
+
+
 // --------------------------------------
 // Exercise 2 - Defining an object. 
 
 // Create an object that has your name and age. 
+
+const jegErMig = {
+    navn: "Tara",
+    alder: 35,
+}
+
+jegErMig.navn = "Morten"; //Kan deklarere/opdatere/ændre værdien udenfor "KEY".
+
+console.log(jegErMig.navn, jegErMig.alder);
+
+const hejHej = `Jeg hedder ${jegErMig.navn} og jeg er ${jegErMig.alder} år gammel`;
+console.log(hejHej);
 
 
 // --------------------------------------
@@ -20,12 +36,32 @@ const stackOverflow = {};
 
 // make a rule called isAllowed and let the value be true
 
+stackOverflow.isAllowed = true;
+
+// Lavet en Key-Value regl oppe ved objektet som indeholder mit navn som værdi.
+//Skiftet Tara til Morten
+
+
 // --------------------------------------
 // Exercise 4 - Remove a property 
 
 const thisSong = { description: "The best song in the world." };
 
 // remove the property "description" and add a property called "about" that should say "Just a tribute." 
+
+// Uden at fjerne, men bare KUN logge "about".
+const about1 = thisSong.about = "Just a tribute." 
+console.log(about1);
+
+
+//Ferner og tilføjer som opgavebeskrivelsen siger.
+const thisSong1 = { description: "The best song in the world." };
+
+delete thisSong1.description;
+thisSong1.about = "Just a tribute." 
+
+//Viser hele objektet, og hvad den indeholder, { about: 'Just a tribute.'}
+console.log(thisSong1);
 
 
 // --------------------------------------
